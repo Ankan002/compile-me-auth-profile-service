fb62d3fed022be693aad925c6cf0e5dd63ab385c
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-02-17 22:42:11 +0530

    feat: :sparkles: add tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      primaryDark: "#1C1B2B",
+      secondaryDark: "#10111A",
+      primaryLight: "#F4F6FA",
+      secondaryLight: "#F1F2F6",
+      primaryOrange: "#FC5830",
+      lightGrey: "#B5BAC7",
+      white: "#FFFFFF",
+      primaryYellow: "#F6BA3D",
+      semiTransparent: "rgba(0, 0, 0, 0.6)",
+      primaryGreen: "#08CD92",
+      primaryRed: "#D61C4E",
+    },
+  },
+  plugins: [],
+};
