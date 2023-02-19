d2511fa576d0d715701cb33dc7c37ca600ef5cab
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-02-19 10:06:10 +0530

    feat: :sparkles: add hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import heroImage from "assets/hero-image.svg";
+import Image from "next/image";
+
 export default function Home() {
     return (
-        <main className="min-h-screen flex flex-col w-full bg-primary-light dark:bg-primary-dark">
-            <h1 className="font-quicksand text-3xl text-primary-dark dark:text-primary-light">
-                Hello World!!
+        <main className="min-h-screen flex flex-col w-full bg-primary-light dark:bg-primary-dark items-center justify-center p-3">
+            <Image src={heroImage} width={300} height={300} alt="" className="w-96 h-96" />
+
+            <h1 className="font-quicksand text-4xl tracking-widest text-primary-orange dark:text-primary-green font-extrabold text-center">
+                COMPILE ME
             </h1>
+
+            <p className="font-fira-code text-2xl tracking-wider text-primary-dark dark:text-primary-light mt-5 md:w-1/3 sm:w-2/3 w-full text-center">
+                Sign in to the world of tech and software
+            </p>
         </main>
     );
 }
