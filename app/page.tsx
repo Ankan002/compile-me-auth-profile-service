6395f80e6fc0d272bd82bac8e4c185c7b4b39a50
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-02-19 13:11:29 +0530

    chore: :zap: optimized image size

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import heroImage from "assets/hero-image.svg";
+import LoginHeroImage from "assets/login-hero-image.png";
 import Image from "next/image";
 
 export default function Home() {
     return (
         <main className="min-h-screen flex flex-col w-full bg-primary-light dark:bg-primary-dark items-center justify-center p-3">
-            <Image src={heroImage} width={300} height={300} alt="" className="w-96 h-96" />
+            <Image src={LoginHeroImage.src} width={400} height={400} alt="" className="w-96 h-96" priority />
 
             <h1 className="font-quicksand text-4xl tracking-widest text-primary-orange dark:text-primary-green font-extrabold text-center">
                 COMPILE ME
